74031f49cd4f48e3bed88df5a072fee555c41ca5
kunsanw <kunsanw@users.noreply.github.com>
2022-12-27 06:42:04 +0800

    Update snell-v3.sh

--- snell-v3.sh
+++ snell-v3.sh
@@ -3,11 +3,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
-apt-get install unzip -y
 cd ~/
-wget --no-check-certificate -O snell.zip https://github.com/kunsanw/snell.sh/archive/refs/tags/dev.zip
-unzip -o snell.zip
- rm -f snell.zip
+wget --no-check-certificate https://github.com/kunsanw/snell.sh/releases/download/dev/snell-server
  chmod +x snell-server
  mv -f snell-server /usr/local/bin/
  if [ -f ${CONF} ]; then
